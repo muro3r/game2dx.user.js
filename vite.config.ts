84c378d7828fbab86d72da4bf1b85c1609c09a5a
muro3r <10064571+muro3r@users.noreply.github.com>
2023-11-13 23:06:29 +0900

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import monkey from 'vite-plugin-monkey'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        name: 'game2dx',
+        namespace: 'muro3r/game2dx',
+        author: 'muro3r',
+        match: ['https://p.eagate.573.jp/game/2dx/*'],
+        icon: 'https://www.google.com/s2/favicons?sz=64&domain=573.jp'
+      }
+    })
+  ]
+})
