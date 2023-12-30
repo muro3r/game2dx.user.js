f9dc723692b2708892ee16068db8a8c180114071
muro3r <10064571+muro3r@users.noreply.github.com>
2023-12-30 23:25:26 +0900

    style(formatting): Apply Biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import monkey from 'vite-plugin-monkey'
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/main.ts',
+      entry: "src/main.ts",
       build: {
-        fileName: 'game2dx.user.js'
+        fileName: "game2dx.user.js",
       },
       userscript: {
-        name: 'game2dx',
-        namespace: 'muro3r/game2dx',
-        author: 'muro3r',
-        match: ['https://p.eagate.573.jp/game/2dx/*'],
-        icon: 'https://www.google.com/s2/favicons?sz=64&domain=573.jp'
-      }
-    })
-  ]
-})
+        name: "game2dx",
+        namespace: "muro3r/game2dx",
+        author: "muro3r",
+        match: ["https://p.eagate.573.jp/game/2dx/*"],
+        icon: "https://www.google.com/s2/favicons?sz=64&domain=573.jp",
+      },
+    }),
+  ],
+});
