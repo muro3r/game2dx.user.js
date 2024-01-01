b3ce95a1e345626cba5062570b8edb3242388ae0
muro3r <10064571+muro3r@users.noreply.github.com>
2024-01-01 21:47:32 +0900

    feat: 師弟システム ビンゴカードの設定値を一時記憶する

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,6 @@
-// @ts-ignore isolatedModules
-console.log("hello world");
+import rememberSelectOption from "./rememberSelectorOption";
+
+// 師弟システム ビンゴカード
+if (location.pathname.match(/rival\/bingo\/card_set_(music|cleartype).html$/)) {
+  rememberSelectOption();
+}
